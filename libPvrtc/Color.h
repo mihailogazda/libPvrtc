adf7be5aa656d8f6763017f6e8cb0c8d18a52946
Mihailo Gazda <mihailogazda@gmail.com>
2015-12-14 00:05:16 +0000

    Min & max colors calculation

--- libPvrtc/Color.h
+++ libPvrtc/Color.h
@@ -14,6 +14,37 @@ namespace libpvrtc
 		ColorType R;
 		ColorType G;
 		ColorType B;
+
+		void SetMin(const ColorRGB &x)
+		{
+			if (x.R < R) {
+				R = x.R;
+			}
+			if (x.G < G) {
+				G = x.G;
+			}
+			if (x.B < B) {
+				B = x.B;
+			}
+		}
+
+		void SetMax(const ColorRGB &x)
+		{
+			if (x.R > R) {
+				R = x.R;
+			}
+			if (x.G > G) {
+				G = x.G;
+			}
+			if (x.B > B) {
+				B = x.B;
+			}
+		}
+
+		unsigned int Normalize()
+		{
+			return (R + G + B) / 3;
+		}
 	};
 
 	struct ColorRGBA : ColorRGB
--- libPvrtc/PVRTCEncoder.cpp
+++ libPvrtc/PVRTCEncoder.cpp
@@ -27,6 +27,52 @@ namespace libpvrtc
 		return success;
 	}
 
+	template<typename T>
+	class Interval
+	{
+	public:
+		T min, max;
+		Interval<T> &operator|=(const T &x)
+		{
+			min.SetMin(x);
+			max.SetMax(x);
+			return *this;
+		}
+	};
+
+	//	Bounding box for ColorRGB
+	typedef Interval<ColorRGB> ColorRgbBB;
+
+
+	static void CalculateBoundingBox(int x, int y, ImageRGB* image, ColorRgbBB& cbb)
+	{
+		ColorRGB center = image->GetPixel(x, y);
+
+		cbb.max = center;
+		cbb.min = center;
+
+		//	Right and bottom
+		cbb |= image->GetPixel(x + 1, y);
+		cbb |= image->GetPixel(x + 1, y + 1);
+
+		//	left and bottom
+		cbb |= image->GetPixel(x - 1, y);
+		cbb |= image->GetPixel(x - 1, y + 1);
+
+		//	top and right
+		cbb |= image->GetPixel(x - 1, y - 1);
+		cbb |= image->GetPixel(x + 1, y - 1);
+
+		//	top and bottom
+		cbb |= image->GetPixel(x, y - 1);
+		cbb |= image->GetPixel(x, y + 1);
+
+		//	xxx  
+		//	xcx
+		//	xxx
+	}
+
+
 	bool PVRTCEncoder::EncodeRGB(const Image* image, ColorType* &encodedData)
 	{
 		int blockCount = (image->Width() * image->Height()) / PVRTC_PIXELS_PER_BLOCK;
@@ -39,12 +85,14 @@ namespace libpvrtc
 			for (int j = 0; j < image->Height() / 4; ++j)
 			{
 				PVRTCFrame_RGB* fr = frames + MortonLUT::GetMortonNumber(i, j);
-				ColorRGB color = img->GetPixel(i * 4, j * 4);
 
-				fr->SetColorA(color);
-				fr->SetColorB(color);
+				ColorRgbBB box;
+				CalculateBoundingBox(i * 4 + 2, j * 4 + 2, img, box);
+
+				fr->SetColorA(box.min);
+				fr->SetColorB(box.max);
 				fr->UsePunchtroughAlpha = 0;
-				fr->ModulationData = 0xFFFFFFFF;
+				fr->ModulationData = 0xAAAAAAAA;
 			}
 		}
 
@@ -63,7 +111,7 @@ namespace libpvrtc
 			for (int j = 0; j < image->Height() / 4; ++j)
 			{
 				PVRTCFrame_RGBA* fr = frames + MortonLUT::GetMortonNumber(i, j);
-				ColorRGBA color = img->GetPixel(i * 4, j * 4);
+				ColorRGBA color = img->GetPixel(i * 4 + 2, j * 4 + 2);
 
 				fr->SetColorA(color);
 				fr->SetColorB(color);
